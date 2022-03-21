35721f4f283d199d46b209fcf362974956f0d949
eslof <eslof2.github@gmail.com>
2022-03-21 15:27:00 +0100

    Base for the player

---
+++ Model/Player/IPlayer.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace Model\Player;
+use Model\Board\IBoard;
+use Model\Direction;
+use Model\Shape\IShape;
+use Model\State\IState;
+
+interface IPlayer extends IState {
+    public function getFd(): int;
+    public function getName(): string;
+    public function getShape(): IShape;
+    public function processMovement(): void;
+    public function processCollision(): void;
+    /** @var null|Closure(IPlayer):void */
+    public function setDeathCallback(?\Closure $onDeath): void; // TODO: abstract the callback pattern
+    public function setBoard(IBoard $board): void;
+    public function setInput(Input $input): void;
+    public function tryInstantiate(?int $x = null, ?int $y = null, ?Direction $direction = null): bool;
+}
---
+++ Model/Player/Input.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+
+namespace Model\Player;
+enum Input: int {
+    case NONE = 0;
+    case LEFT = 1;
+    case RIGHT = 2;
+}
