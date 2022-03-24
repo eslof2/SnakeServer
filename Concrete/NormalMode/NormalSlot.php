9c4aed5ae3bd6e409cffdece245124ce435e61a7
eslof <eslof2.github@gmail.com>
2022-03-24 19:08:39 +0100

    internal misuse exception for unconsolidated collisions
    for now I'm going to try to work with this restraint to see if it makes sense, you can have any amount of entities in a slot but for this game mode as it currently stands at serialize time (after movement and collision) there should only be one entity left per slot

--- Concrete/NormalMode/NormalSlot.php
+++ Concrete/NormalMode/NormalSlot.php
@@ -1,6 +1,13 @@
 <?php declare(strict_types=1);
 
 namespace Concrete\NormalMode;
+use Model\InternalMisuseException;
 use Model\Slot\SlotBase;
 
-class NormalSlot extends SlotBase { }
+class NormalSlot extends SlotBase {
+    public function jsonSerialize(): array {
+        if (count($this->entities) > 1) throw new InternalMisuseException("Collisions are not consolidated at serialize-time.");
+        return $this->entities; //empty array takes up less space in json than null or false
+        //return reset($this->entities) otherwise we'd use this
+    }
+}
