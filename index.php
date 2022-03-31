392b0516aba7ca21bb7930125822cec3a742fd32
eslof <eslof2.github@gmail.com>
2022-03-31 11:16:32 +0200

    comments on enabling ssl

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $game = new NormalGame(30, 30, Config::CONCURRENT_MAX);
 $process = new GameProcess();
 $server = new GameServer();
 
-$webSocket = new Server("localhost", 80);
+$webSocket = new Server("localhost", 80); //, int $mode = SWOOLE_PROCESS, int $sockType = SWOOLE_SOCK_TCP | SWOOLE_SSL);
 //$server->set([
 //    'ssl_cert_file' => __DIR__ . '/config/example.com+5.pem',
 //    'ssl_key_file' => __DIR__ . '/config/example.com+5-key.pem'
